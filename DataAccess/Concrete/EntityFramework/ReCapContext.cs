e7ba8bd75a684739cc02d68427194e0797578d7b
hlf-u <uludaghalef1808@gmail.com>
2021-03-02 02:12:01 +0300

    11.Gün 1.Ödev

--- DataAccess/Concrete/EntityFramework/ReCapContext.cs
+++ DataAccess/Concrete/EntityFramework/ReCapContext.cs
@@ -10,7 +10,7 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Hlf;Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=RecapDb;Trusted_Connection=true");
         }
 
         public DbSet<Car> Cars { get; set; }
